9f3051eaf2f9bc3414d11330477d143094b79735
c4rlo5Riv3ra <jcriveraguadalupe@gmail.com>
2025-04-22 11:10:53 -0500

    fix model Usuario

--- app/models.py
+++ app/models.py
@@ -9,3 +9,8 @@ class Usuario(models.Model):
     id_rol = models.CharField(max_length=60, null=False)
     estado = models.IntegerField(choices=estado_Usuario, default=estado_Usuario.ACTIVO)
 
+    class Meta:
+        db_table = "usuario"
+        ordering = ['id']
+
+
