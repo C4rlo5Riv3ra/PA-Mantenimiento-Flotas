76e5e1095430c7f6355da706948e7ce6bcd01bc3
c4rlo5Riv3ra <jcriveraguadalupe@gmail.com>
2025-04-25 18:02:51 -0500

    fixed: models created

--- app/models.py
+++ app/models.py
@@ -1,35 +1,59 @@
 from django.db import models
-from proyect.choices import estado_Usuario
+from proyect.choices import *
 
+# Create your models here.
+
+# Modelo Persona
 class Persona(models.Model):
-    nombre = models.CharField(max_length=50)
-    apellidos = models.CharField(max_length=50)
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
     doc_identity = models.CharField(max_length=8, unique=True)
-    telefono = models.CharField(max_length=10)
-    direccion = models.CharField(max_length=80)
+    phone = models.CharField(max_length=10)
+    address = models.CharField(max_length=80)
 
     class Meta:
         db_table = "persona"
+        ordering = ['id']
 
-class Rol(models.Model):
-    nombre = models.CharField(max_length=20, unique=True)
-
-    class Meta:
-        db_table = "rol"
-
-class EstadoUsuario(models.Model):
-    nombre = models.CharField(max_length=20, unique=True)
-
-    class Meta:
-        db_table = "estado_usuario"
-
+# Modelo Usuario
 class Usuario(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=100)
-    persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
-    rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
-    estado = models.ForeignKey(EstadoUsuario, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True)
+    email = models.CharField(max_length=60, null=False)
+    password = models.CharField(max_length=60, null=False)
+    id_rol = models.IntegerField(choices=rol)
+    id_person = models.ForeignKey(Persona, on_delete=models.RESTRICT, null=False, related_name='usuario')
+    state = models.IntegerField(choices=estado_Usuario, default=estado_Usuario.ACTIVO)
+    last_date_support = models.DateField(null=True, blank=True)
+    next_date_support = models.DateField(null=True, blank=True)
+
 
     class Meta:
         db_table = "usuario"
+        ordering = ['id']
+
+# Modelo Tipo Vehiculo
+class TipoVehiculo(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=30, unique=True)
+
+    class Meta:
+        db_table = 'tipo_vehiculo'
+        ordering = ['id']
+
+# Modelo Vehiculo
+class Vehiculo(models.Model):
+    id = models.UUIDField(primary_key=True)
+    placa = models.CharField(max_length=15, unique=True)
+    marca = models.CharField(max_length=25)
+    modelo = models.CharField(max_length=20)
+    year = models.IntegerField()
+    id_tipo_vehiculo = models.ForeignKey(TipoVehiculo, on_delete=models.RESTRICT, null=False, related_name='tipo_vehiculo')
+    state = models.IntegerField(choices=estado_Vehiculo, default=estado_Vehiculo.NUEVO)
+    current_kilometers = models.FloatField(default=0)
+    last_date_support = models.DateField(null=True, blank=True)
+    next_date_support = models.DateField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'vehiculo'
+        ordering = ['id']
