56a5fd32f65d666d2bb45f57e863d262ba1ba2fb
c4rlo5Riv3ra <jcriveraguadalupe@gmail.com>
2025-04-25 18:02:12 -0500

    add: 2 class rol and vehicle

--- proyect/choices.py
+++ proyect/choices.py
@@ -12,6 +12,11 @@ class estado_Conductor(models.IntegerChoices):
     DE_BAJA = 3, 'De baja'
     SUSPENDIDO = 4, 'Suspendido'
 
+# clase vehiculo aun no esta implementada en la base de datos
+class estado_Vehiculo(models.IntegerChoices):
+    NUEVO = 1, 'Nuevo'
+    DETERIORADO = 2, 'Deteriorado'
+
 class estado_Asignacion(models.IntegerChoices):
     ACTIVA = 1, 'Activa'
     FINALIZADA = 2, 'Finalizada'
@@ -23,3 +28,8 @@ class estado_Alerta(models.IntegerChoices):
     PENDIENTE = 2, 'Pendiente'
     CANCELADA = 3, 'Cancelada'
     VENCIDA = 4, 'Vencida'
+
+class rol(models.IntegerChoices):
+    CONDUCTOR = 1, 'Conductor'
+    TECNICO = 2, 'Tecnico'
+    ADMIN = 3, 'Admin'
