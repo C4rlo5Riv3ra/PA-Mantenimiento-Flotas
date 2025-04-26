271ec042f8528fedb76cef9aa62127736d60caf6
JUAN CARLOS <cjimenezjuancar@uss.edu.pe>
2025-04-26 16:42:54 -0500

    Agregamos el modelo Conductor, Asignacion, TipoMantenimiento y Mantenimiento

--- app/models.py
+++ app/models.py
@@ -57,3 +57,51 @@ class Vehiculo(models.Model):
     class Meta:
         db_table = 'vehiculo'
         ordering = ['id']
+
+# Modelo Conductor
+class Conductor(models.Model):
+    id = models.UUIDField(primary_key=True)
+    id_persona = models.ForeignKey(Persona, on_delete=models.RESTRICT, null=False, related_name='persona')
+    licence_drive = models.CharField(max_length=30)
+    date_entry = models.DateField()
+    state = models.IntegerField(choices=estado_Conductor, default=estado_Conductor.NUEVO)
+
+    class Meta:
+        db_table = 'conductor'
+        ordering = ['id']
+
+# Modelo Asignacion
+class Asignacion(models.Model):
+    id = models.UUIDField(primary_key=True)
+    id_vehiculo = models.ForeignKey(Vehiculo, on_delete=models.RESTRICT, null=False, related_name='vehiculo')
+    id_conductor = models.ForeignKey(Conductor, on_delete=models.RESTRICT, null=False, related_name='conductor')
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    state = models.IntegerField(choices=estado_Asignacion, default=estado_Asignacion.NUEVO)
+
+    class Meta:
+        db_table = 'asignacion'
+        ordering = ['id']
+
+# Modelo TipoMantenimiento
+class TipoMantenimiento(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=200, unique=True)
+
+    class Meta:
+        db_table = 'tipo_mantenimiento'
+        rdering = ['id']
+
+# Modelo Mantenimiento
+class Mantenimiento(models.Model):
+    id = models.UUIDField(primary_key=True)
+    id_vehiculo = models.ForeignKey(Vehiculo, on_delete=models.RESTRICT, null=False, related_name='vehiculo')
+    description = models.CharField(max_length=250)
+    date = models.DateField()
+    kilometraje = models.FloatField()
+    costo = models.FloatField()
+    workshop = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'mantenimiento'
+        rdering = ['id']
